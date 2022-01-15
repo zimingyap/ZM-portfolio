d9147958374b95ca0a62283ea4d1a7bac407e649
Ziming Yap <C00425108@louisiana.edu>
2022-01-15 04:52:05 -0600

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "zmportfolio23ad865a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
+import "./App.css";
 // import About from "./components/About";
 // import Contact from "./components/Contact";
 // import Navbar from "./components/Navbar";
 // import Projects from "./components/Projects";
 // import Skills from "./components/Skills";
+// import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
     return (
-        <div className="app">
-            <h1>Zimingyap.comgg</h1>
-          {/* <Navbar />
-          <About />
-          <Projects />
-          <Skills />
-          <Contact /> */}
-        </div>
+        <Authenticator>
+          {({ signOut, user }) => (
+            <div className="App">
+              <p>
+                Hey {user.username}, welcome to my channel, with auth!
+              </p>
+              <button onClick={signOut}>Sign out</button>
+            </div>
+          )}
+        </Authenticator>
       );
 }
 
